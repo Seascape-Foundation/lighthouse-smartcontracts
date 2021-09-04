2821410127ab209b83e0f82eb7b8490ff1aa0192
ahmetson <ahmetson@zoho.com>
2021-09-04 19:52:47 +0700

    Init the Hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  console.log(hre);
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  networks: {
+    hardhat: {
+      mining: {
+        auto: false,
+        interval: 1000
+      }
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_KEY
+  },
+  solidity: "0.8.0"
+};
