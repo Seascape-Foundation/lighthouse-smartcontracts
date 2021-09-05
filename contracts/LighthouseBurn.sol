eb6be85a23ce8c85321344404887a26318754faf
ahmetson <ahmetson@zoho.com>
2021-09-05 19:58:58 +0700

    When nft burns, compensation or pcc is used to.

--- contracts/LighthouseBurn.sol
+++ contracts/LighthouseBurn.sol
@@ -43,11 +43,18 @@ contract LighthouseBurn is Ownable {
         uint256 startTime;
     }
 
+    /// @notice Some PCC goes to staking contract
+    /// @dev PCC address => reserve amount
+    mapping(address => uint256) public stakeReserves;
+    /// @notice The contract to where the staked tokens will go
+    address public staker;
+
     mapping(uint256 => Project) public projects;
 
     /// @notice Check whether the user minted nft for the project or not
     mapping(uint256 => mapping(address => uint256)) public mintedNfts;
 
+    event SetStaker(address indexed staker);
     event AddProject(uint256 indexed projectId, uint256 prefundPool, uint256 auctionPool, uint256 prefundCompensation, uint256 auctionCompensation, address indexed lighthouse, uint256 startTime);
     event AddPCC(uint256 indexed projectId, address indexed pcc);
     event BurnForPCC(uint256 indexed projectId, address indexed lighthouse, uint256 indexed nftId, address owner, address pcc, uint256 allocation);
@@ -61,6 +68,38 @@ contract LighthouseBurn is Ownable {
         lighthouseTier      = LighthouseTier(_lighthouseTier);
         lighthouseProject   = LighthouseProject(_project);
         crowns              = CrownsInterface(_crowns);
+    }
+
+    function setStaker(address _staker) external onlyOwner {
+        require(_staker != address(0), "Lighthouse: ZERO_ADDRESS");
+
+        staker = _staker;
+
+        emit SetStaker(_staker);
+    }
+
+    function transferStakeReserve(address pccAddress, uint256 amount) external {
+        require(amount > 0, "Lighthouse: ZERO_PARAMETER");
+        require(pccAddress != address(0) && staker != address(0), "Lighthouse: NO_STAKER_ADDRESS");
+        require(msg.sender == owner() || msg.sender == staker, "Lighthouse: FORBIDDEN");
+        require(stakeReserves[pccAddress] > 0, "Lighthouse: INVALID_PCC");
+        require(stakeReserves[pccAddress] >= amount, "Lighthouse: NOT_ENOUGH_BALANCE");
+
+        CrownsInterface pcc = CrownsInterface(pccAddress);
+        pcc.transferFrom(address(this), staker, amount);
+
+        stakeReserves[pccAddress] = stakeReserves[pccAddress] - amount;
+    }
+
+    function transferMaxReserve(address pccAddress) external {
+        require(pccAddress != address(0) && staker != address(0), "Lighthouse: NO_STAKER_ADDRESS");
+        require(msg.sender == owner() || msg.sender == staker, "Lighthouse: FORBIDDEN");
+        require(stakeReserves[pccAddress] > 0, "Lighthouse: INVALID_PCC");
+
+        CrownsInterface pcc = CrownsInterface(pccAddress);
+        pcc.transferFrom(address(this), staker, stakeReserves[pccAddress]);
+
+        stakeReserves[pccAddress] = 0;
     }
 
     /// @notice add a new project to the IDO project.
@@ -113,49 +152,61 @@ contract LighthouseBurn is Ownable {
     //
     //////////////////////////////////////////////////////////////////////
 
-    /// 100k, 10k cws, 10:1
-    // @todo compensation cws is burnt to spend it.
     function burnForPcc(uint256 projectId, uint256 nftId) external {
-        Project storage project = projects[projectId];
-        require(project.pcc != address(0), "Lighthouse: NO_PCC");
+        address pccAddress = lighthouseProject.pcc(projectId);
+        require(pccAddress != address(0), "Lighthouse: NO_PCC");
+        require(lighthouseProject.allocationCompensationInitialized(projectId), "Lighthouse: ALLOCATION_NOT_INITIALIZED_YET");
 
-        LighthouseNft nft = LighthouseNft(project.lighthouse);
+        CrownsInterface pcc = CrownsInterface(pccAddress);
+
+        // Nft address after initiation of Allocation should work.
+        LighthouseNft nft = LighthouseNft(lighthouseProject.nft(projectId));
         require(nft.ownerOf(nftId) == msg.sender, "Lighthouse: NOT_NFT_OWNER");
         require(nft.mintType(nftId) <= 2, "Lighthouse: FORBIDDEN_MINT_TYPE");
 
-        uint256 allocation = nft.getAllocation(nftId) / SCALER;
+        uint256 allocation = nft.scaledAllocation(nftId) / SCALER;
+        uint256 compensation = nft.scaledCompensation(nftId / SCALER);
         require(allocation > 0, "Lighthouse: NFT_ZERO_ALLOCATION");
+
+        require(pcc.balanceOf(address(this)) >= allocation, "Lighthouse: NOT_ENOUGH_PCC");
+        require(crowns.balanceOf(address(this)) >= compensation, "Lighthouse: NOT_ENOUGH_CROWNS");
 
         nft.burn(nftId);
 
-        IERC20 pcc = IERC20(project.pcc);
         pcc.transferFrom(address(this), msg.sender, allocation);
+        crowns.spendFrom(address(this), compensation);
 
-        emit BurnForPCC(projectId, project.lighthouse, nftId, msg.sender, project.pcc, allocation);
+        emit BurnForPCC(projectId, lighthouseProject.nft(projectId), nftId, msg.sender, pccAddress, allocation);
     }
 
-    // @todo transfer allocation PCC in ration to CWS to the staking pool PCC.
     function burnForCws(uint256 projectId, uint256 nftId) external {
-        Project storage project = projects[projectId];
-        require(project.pcc != address(0), "Lighthouse: NO_PCC");
+        address pccAddress = lighthouseProject.pcc(projectId);
+        require(pccAddress != address(0), "Lighthouse: NO_PCC");
+        require(lighthouseProject.allocationCompensationInitialized(projectId), "Lighthouse: ALLOCATION_NOT_INITIALIZED_YET");
 
-        LighthouseNft nft = LighthouseNft(project.lighthouse);
+        CrownsInterface pcc = CrownsInterface(pccAddress);
+
+        LighthouseNft nft = LighthouseNft(lighthouseProject.nft(projectId));
         require(nft.ownerOf(nftId) == msg.sender, "Lighthouse: NOT_NFT_OWNER");
         require(nft.mintType(nftId) <= 2, "Lighthouse: FORBIDDEN_MINT_TYPE");
 
-        uint256 compensation = nft.getCompensation(nftId);
+        uint256 allocation = nft.scaledAllocation(nftId) / SCALER;
+        uint256 compensation = nft.scaledCompensation(nftId) / SCALER;
         require(compensation > 0, "Lighthouse: NFT_ZERO_COMPENSATION");
+
+        require(pcc.balanceOf(address(this)) >= allocation, "Lighthouse: NOT_ENOUGH_PCC");
+        require(crowns.balanceOf(address(this)) >= compensation, "Lighthouse: NOT_ENOUGH_CROWNS");
 
         nft.burn(nftId);
 
         crowns.transferFrom(address(this), msg.sender, compensation);
+        stakeReserves[pccAddress] = stakeReserves[pccAddress] + allocation;
 
-        emit BurnForCWS(projectId, project.lighthouse, nftId, msg.sender, compensation);
+        emit BurnForCWS(projectId, lighthouseProject.nft(projectId), nftId, msg.sender, compensation);
     }
 
     // @todo separated contract
     /// need to ask: could it be any project. or user has to choose a certain project for burning this nft.
-    // @todo any nft.
     function burnForProject(uint256 projectId, uint256 anotherProjectId, address nftAddress, uint256 nftId) external {
 
     }
