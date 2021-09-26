7f5a4604f047a6cd7070bbfb6378acf134430164
ahmetson <ahmetson@zoho.com>
2021-09-26 18:21:29 +0700

    Add test contract for PCC testing

---
+++ contracts/Test.sol
@@ -0,0 +1,11 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GLDToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Elements", "ELM") {
+        _mint(msg.sender, initialSupply);
+    }
+}
