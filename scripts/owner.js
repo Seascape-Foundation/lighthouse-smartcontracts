52be689eca667a248be2c975be291c44bee59082
ahmetson <ahmetson@zoho.com>
2022-01-13 14:36:45 +0700

    Fix the owner changing

--- scripts/owner.js
+++ scripts/owner.js
@@ -5,6 +5,7 @@ const chalk               = require("chalk");
 let cliOwner              = require('./cli/owner')
 let cliConfirm            = require('./cli/confirm')
 let cliGas                = require('./cli/gas');
+const gas = require("./cli/gas");
 
 async function main() {
   clear();
@@ -12,11 +13,13 @@ async function main() {
     // We get the contract to deploy
     const Project        = await ethers.getContractFactory("LighthouseProject");
 
-    let deployer      = await ethers.getSigner();
+    const accounts = await hre.ethers.getSigners();
+    console.log(`Transfer from ${accounts[0].address} to ${accounts[1].address}`)
 
-    let address       = await cliOwner.inputAddress();
-
-    console.log(`Signing by ${deployer.address}`);
+    // let address       = await cliOwner.inputAddress();
+    let address = "0x27d72484f1910f5d0226afa4e03742c9cd2b297a";
+    let newOwner = "0x80Fb89Ac96600a53b402324B24017F458dF1c6Ed";
+    // 200000000000 wei
 
     let project = await Project.attach(address);
 
@@ -28,12 +31,12 @@ async function main() {
 
     await cliConfirm.inputConfirm(changeTitle, {});
 
-    if (owner.toLowerCase() != deployer.address.toLocaleLowerCase()) {
-      console.error(chalk.red(`The current user ${deployer.address} and the owner of contract ${owner} doesnt match!`));
+    if (owner.toLowerCase() != accounts[1].address.toLocaleLowerCase()) {
+      console.error(chalk.red(`The current user ${accounts[1].address} and the owner of contract ${owner} doesnt match!`));
       process.exit(1);
     }
 
-    let newOwner = await cliOwner.inputNewOwner();
+    // let newOwner = await cliOwner.inputNewOwner();
     let gasPrice = await cliGas.inputGasPrice();
 
     let newOwnerTitle = `Are you sure you?`
@@ -44,7 +47,15 @@ async function main() {
     };
     await cliConfirm.inputConfirm(newOwnerTitle, newOwnerParams);
 
-  let transferTx = await project.transferOwnership(newOwner, {from: deployer.address, gasPrice});
+    // let sendTx = await accounts[0].sendTransaction({
+    //   to:  accounts[1].address,
+    //   value: ethers.utils.parseEther("0.05"),
+    //   gasPrice: gasPrice
+    // });
+    // await sendTx.wait();
+    // console.log(`Send the tx.`);
+
+  let transferTx = await project.connect(accounts[1]).transferOwnership(newOwner, {gasPrice: gasPrice});
   console.log(`Changing owner was successful! Txid: ` + chalk.blue(transferTx.hash))
 }
 
