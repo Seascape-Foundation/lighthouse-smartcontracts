7d8778c3181d60ae1e59e13c3177712e8eac9701
Görkem Erol <westdabestdb@gmail.com>
2023-06-13 16:57:27 +0300

    fix registration deployment

--- scripts/deploy/registration.js
+++ scripts/deploy/registration.js
@@ -1,10 +1,10 @@
-const chalk                   = require("chalk");
-const clear                   = require("clear");
+const chalk = require("chalk");
+const clear = require("clear");
 
-const { ethers }              = require("hardhat");
-let { addressOf, alias }      = require('../addresses');
-let cliGas                    = require('../cli/gas');
-let cliConfirm                = require('../cli/confirm');
+const { ethers } = require("hardhat");
+let { addressOf, alias } = require('../addresses');
+let cliGas = require('../cli/gas');
+let cliConfirm = require('../cli/confirm');
 
 async function main() {
   clear();
@@ -12,16 +12,16 @@ async function main() {
   console.log(chalk.blue(`Welcome to the deployment of Registration Interface`));
 
   // We get the contract to deploy
-  const Registration  = await ethers.getContractFactory("LighthouseRegistration");
+  const Registration = await ethers.getContractFactory("LighthouseRegistration");
 
-  let deployer        = await ethers.getSigner();
-  let chainID         = await deployer.getChainId();
+  let deployer = await ethers.getSigner();
+  let chainID = await deployer.getChainId();
 
-  let gasPrice        = await cliGas.inputGasPrice();
+  let gasPrice = await cliGas.inputGasPrice();
 
   // Constructor arguments
-  let tierAddress     = await addressOf(chainID, alias.TIER_WRAPPER);
-  let projectAddress  = await addressOf(chainID, alias.PROJECT);
+  let tierAddress = await addressOf(chainID, alias.TIER_WRAPPER);
+  let projectAddress = await addressOf(chainID, alias.PROJECT);
 
   let title = `Please confirm the parameters...`;
   let params = {
@@ -34,16 +34,16 @@ async function main() {
   await cliConfirm.inputConfirm(title, params);
 
   // deploy registration interface
-  let registration              = await Registration.deploy(tierAddress, projectAddress);  
+  let registration = await Registration.deploy(projectAddress, chainID);
 
   console.log("Lighthouse Registration deployed to ", chalk.green(registration.address), ' Txid ', chalk.blue(registration.deployTransaction.hash));
 
   console.log(chalk.green(`Now, please insert it into the /scripts/addresses.js and update the docs.seascape.network/`));
 }
-  
+
 main()
-.then(() => process.exit(0))
-.catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
